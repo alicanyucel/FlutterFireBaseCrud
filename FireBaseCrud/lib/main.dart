6f0847b645d501b1ef97dce8b1e7a942c128895d
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-03-30 11:52:19 +0300

    devam

--- FireBaseCrud/lib/main.dart
+++ FireBaseCrud/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebasecrud/pages/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Firebase Crud', debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home()
     );
   }
 }
--- FireBaseCrud/lib/pages/home.dart
+++ FireBaseCrud/lib/pages/home.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
-class Home extends StatefulWidget{
+
+class Home extends StatefulWidget {
   const Home({super.key});
+
   @override
-  State<Home> createState()=>_HomeState();
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton:FloatingActionButton(onPressed:(){},child:Icon(Icons.add),),
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Flutter",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              " Firebase Crud Alican",
+              style: TextStyle(
+                color: Colors.orange,
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            // İçerik buraya eklenecek
+          ],
+        ),
+      ),
+
+    );
   }
-  class _HomeState extends State<Home> {
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold();
-    }
-  }
+}
